983776add49cd04824eee8e65bb2a42f1fa6b5b6
Ezio Alves <ezio.freire@ctolicasc.org.br>
2020-12-07 09:19:56 -0300

    second commit

---
+++ AppPokedex/app/src/main/java/com/example/apppokedex/FeedEntry.java
@@ -0,0 +1,15 @@
+package com.example.apppokedex;
+
+public class FeedEntry {
+    private int id;
+    private int num;
+    private String name;
+    private String img;
+    private String type;
+    private float heigth;
+    private float weigth;
+    private String candy;
+
+
+
+}
